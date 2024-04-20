7e4455397cd1aab416ae3e7114b874d22e589cc5
Phickayor <jetawof@gmail.com>
2024-04-20 08:01:58 +0100

    Tailwind font and colors configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,17 +4,31 @@ const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))"
       },
-    },
+      fontFamily: {
+        mulish: ["var(--font-mulish)"],
+        petrona: ["var(--font-petrona)"],
+        cabin: ["var(--font-cabin)"]
+      },
+      colors: {
+        lightBlue: "#193AAE",
+        darkBlue: "#03134B",
+        primaryHeader: "#212121",
+        secHeader: "#202020",
+        offWhite: "#E3E3E3",
+        primaryText: "#787878",
+        secText: "#565656"
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
 export default config;
